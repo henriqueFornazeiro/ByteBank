d01d3dc49f7cf2a680238bb3cb94389aba2031d2
HenriqueFornazeiro <HenriqueFornazeiro@LAPTOP-LHPD57HH>
2021-03-27 12:49:58 -0300

    Namespace composição e null.

---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente cliente = new Cliente();
+            cliente.nome = "Henrique";
+            cliente.cpf = "123456789-01";
+            cliente.profissao = "Programador";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = cliente;
+            conta.saldo = 500;
+            conta.agencia = 123;
+            conta.numeroConta = 123456;
+
+            Console.WriteLine(cliente.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+
+        }
+    }
+}
