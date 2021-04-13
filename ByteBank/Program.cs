679a17d59425b27932766cc50e68de01e749a1a0
HenriqueFornazeiro <HenriqueFornazeiro@LAPTOP-LHPD57HH>
2021-04-13 20:10:41 -0300

    Iniciando resoluções do problema do cliente

---
+++ ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = "Henrique";
+            funcionario.CPF = "12345679-89";
+            funcionario.Salario = 2000;
+
+            gerenciador.Registrar(funcionario);
+
+            Console.WriteLine(funcionario.Nome);
+
+            Diretor diretor = new Diretor();
+            diretor.Nome = "Roberta";
+            diretor.Salario = 5000;
+
+            Console.WriteLine(diretor.Nome);
+            Console.WriteLine(diretor.GetBonificacao());
+
+
+            Console.ReadLine();
+        }
+    }
+}
